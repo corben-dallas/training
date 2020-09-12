63d5e57d3284109d5ddfab5a5df477d3406c9b1e
albert_u <albert.u1994@gmail.com>
2020-09-12 19:55:43 +0300

    Updates

---
+++ precache-manifest.996380a1dc130dcfd35ea6dd5ba4cd92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae8820099cf7c89322b1913b050bfb4",
+    "url": "/training/index.html"
+  },
+  {
+    "revision": "eef9bc575828513ee5b0",
+    "url": "/training/static/css/main.9b3ce10c.chunk.css"
+  },
+  {
+    "revision": "67a0af48ee2e2468b2cf",
+    "url": "/training/static/js/2.afd7501e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/training/static/js/2.afd7501e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef9bc575828513ee5b0",
+    "url": "/training/static/js/main.dbc89caa.chunk.js"
+  },
+  {
+    "revision": "00e5da6574c4ff481f25",
+    "url": "/training/static/js/runtime-main.dc698adb.js"
+  }
+]);
